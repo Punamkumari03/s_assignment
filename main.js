94d7557b26bc7d591e7257aa23ac1fd432634b2e
Punamkumari03 <punamgupta250602@gmail.com>
2023-02-05 00:03:25 +0530

    delete from localstorage

--- main.js
+++ main.js
@@ -1,16 +1,53 @@
-function addData(){
+var ul=document.getElementById('item')
+ul.addEventListener('click', removeItem);
+function addData(event){
+    event.preventDefault()
     var userName=document.getElementById('name').value;
     var userEmail=document.getElementById('email').value;
-    // localStorage.setItem('Name',userName)
-    // localStorage.setItem('Email',userEmail)
+    var userPhone=document.getElementById('phone').value;
+  
+   
+    var li = document.createElement('li');
+    li.className = 'l_items';
+    li.appendChild(document.createTextNode(userName+'--'+userEmail+'--'+userPhone));
+   
+    var deleteBtn = document.createElement('button');
+    deleteBtn.className = 'li_btn';
+    deleteBtn.appendChild(document.createTextNode('delete'));
+    li.appendChild(deleteBtn);
+    ul.appendChild(li);
+    
+
     let myObj ={
         name:userName,
-        email:userEmail    
+        email:userEmail ,
+        phone:userPhone   
     }
     let myObj_serialized=JSON.stringify(myObj);
-    localStorage.setItem('myObj',myObj_serialized)
+    localStorage.setItem(myObj.email,myObj_serialized)
+   
 
     // localStorage.setItem('Name',myObj.name)
     // localStorage.setItem('Email',myObj.email)
 }
 
+// Remove item
+function removeItem(event){
+    if(event.target.classList.contains('li_btn')){
+      
+        var li = event.target.parentElement;
+        ul.removeChild(li);
+        var userName=document.getElementById('name').value;
+        var userEmail=document.getElementById('email').value;
+        var userPhone=document.getElementById('phone').value;
+        let myObj ={
+            name:userName,
+            email:userEmail ,
+            phone:userPhone   
+        }
+        let myObj_serialized=JSON.stringify(myObj);
+        localStorage.removeItem(myObj.email,myObj_serialized)
+      
+    }
+
+  }
